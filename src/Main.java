27fd25321bf2e372e94edeb6605fc058d2e6c33a
ismael vas <155621485+isma444@users.noreply.github.com>
2024-01-15 11:53:35 +0100

    Exercice 2  Suites de nombres

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,14 @@ public class Main {
 
         enumerate_even(maxNumber);
 
+        System.out.println();
+
+        superior_square(63);
+
+        System.out.println();
+        inferior_square(30);
+        System.out.print(" ");
+        inferior_square(63);
     }
     static public void enumerate_while(int max) {
         int i = 0;
@@ -41,5 +49,16 @@ public class Main {
             }
             i++;
         }
+    }
+
+    static public void superior_square(int number){
+
+       double result = Math.ceil(Math.sqrt(number));
+       System.out.print(Math.round(result*result));
+    }
+
+    static public void inferior_square(int number){
+        double result = Math.floor(Math.sqrt(number));
+        System.out.print(Math.round(result*result));
     }
 }
